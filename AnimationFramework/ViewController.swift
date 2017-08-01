56a819a4e52d9e0a178f00a1397b4da90d6205e4
Jordan Dixon <jordan.dixon@mubaloo.com>
2017-08-01 08:00:58 +0100

    Initial Commit

---
+++ AnimationFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationFramework
+//
+//  Created by Jordan.Dixon on 01/08/2017.
+//  Copyright © 2017 Mubaloo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
